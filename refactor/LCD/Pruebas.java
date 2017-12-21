0d7ad6c0162ce7a524c79e57f7f8d15b418af382
Guillermo Calderón Mantilla <guillermojose.calderon@gmail.com>
2017-12-21 20:20:21 +0000

    Delete Pruebas.java

--- refactor/LCD/Pruebas.java
+++
@@ -1,9 +0,0 @@
-package refactor.LCD;
-
-public class Pruebas {
-    public static void main(String[] args) {
-        Impresor impresora=new Impresor("2345", 4, 3);
-        impresora.imprimirMatriz();
-    }
-
-}
